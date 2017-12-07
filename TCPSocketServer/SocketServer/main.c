84d01af0709ae3a4a320a30e0cb80cd31ef429e6
ilknur <ilknurbisirici@gmail.com>
2017-12-07 14:14:06 +0300

    socket oluşturuldu.

--- TCPSocketServer/SocketServer/main.c
+++ TCPSocketServer/SocketServer/main.c
@@ -1,8 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*
+    C socket server example, handles multiple clients using threads
+*/
 
-int main()
+#include<stdio.h>
+#include<string.h>    //strlen
+#include<stdlib.h>    //strlen
+#include<sys/socket.h>
+#include<arpa/inet.h> //inet_addr
+#include<unistd.h>    //write
+#include<pthread.h> //for threading , link with lpthread
+
+//the thread function
+void *connection_handler(void *);
+
+int main(int argc , char *argv[])
 {
-    printf("Hello world!\n");
-    return 0;
+    int socket_desc , client_sock , c , *new_sock;
+    struct sockaddr_in server , client;
+
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
 }
+
